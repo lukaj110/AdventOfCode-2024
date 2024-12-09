0f5e5d286cda23eaeed8496b1bda07367f11e49e
lukaj110 <luka.jovanovic.lj01@gmail.com>
2024-12-09 01:11:23 +0100

    day 7 part 2

--- 07/solution.ts
+++ 07/solution.ts
@@ -10,26 +10,41 @@ const result = inputText.map((e) => e[0]).map((e) => parseInt(e));
 
 // Part 1
 
-const operations = ["*", "+"];
+function generateStringCombinations(
+  options: string[],
+  length: number
+): string[] {
+  const combinations: string[] = [];
+
+  function generateCombos(currentCombo: string, remainingLength: number): void {
+    if (remainingLength === 0) {
+      combinations.push(currentCombo);
+      return;
+    }
+
+    for (const option of options) {
+      generateCombos(currentCombo + option, remainingLength - 1);
+    }
+  }
+
+  generateCombos("", length);
+
+  return combinations;
+}
 
 let totalCalibrationResult = 0;
+
+let options = ["+", "*"];
 
 for (let i = 0; i < result.length; i++) {
   const currentResult = result[i];
 
   const currentNumbers = numbers[i].split(" ").map((e) => parseInt(e));
 
-  const allCombinations: string[] = [];
-
-  for (let j = 0; j < 1 << (currentNumbers.length - 1); j++) {
-    const binaryString = (j >>> 0)
-      .toString(2)
-      .padStart(currentNumbers.length - 1, "0")
-      .replaceAll("0", operations[0])
-      .replaceAll("1", operations[1]);
-
-    allCombinations.push(binaryString);
-  }
+  const allCombinations: string[] = generateStringCombinations(
+    options,
+    currentNumbers.length - 1
+  );
 
   let found = false;
 
@@ -61,3 +76,53 @@ for (let i = 0; i < result.length; i++) {
 console.log(totalCalibrationResult);
 
 // Part 2
+
+totalCalibrationResult = 0;
+
+options = ["+", "*", "|"];
+
+for (let i = 0; i < result.length; i++) {
+  const currentResult = result[i];
+
+  const currentNumbers = numbers[i].split(" ").map((e) => parseInt(e));
+
+  const allCombinations: string[] = generateStringCombinations(
+    options,
+    currentNumbers.length - 1
+  );
+
+  let found = false;
+
+  allCombinations.forEach((combination) => {
+    if (found) return;
+
+    let tempNumbers = [...currentNumbers];
+
+    combination.split("").forEach((operation) => {
+      const firstNumber = tempNumbers.shift()!;
+
+      const secondNumber = tempNumbers.shift()!;
+
+      if (operation === "|") {
+        tempNumbers = [
+          parseInt(`${firstNumber}${secondNumber}`),
+          ...tempNumbers,
+        ];
+      } else {
+        tempNumbers = [
+          operation === "+"
+            ? firstNumber + secondNumber
+            : firstNumber * secondNumber,
+          ...tempNumbers,
+        ];
+      }
+    });
+
+    if (currentResult === tempNumbers[0]) {
+      totalCalibrationResult += currentResult;
+      found = true;
+    }
+  });
+}
+
+console.log(totalCalibrationResult);
