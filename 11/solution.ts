f59a8bb586fffd09542f173a187434f6af37ee40
lukaj110 <luka.jovanovic.lj01@gmail.com>
2024-12-12 02:20:54 +0100

    cleaner code

--- 11/solution.ts
+++ 11/solution.ts
@@ -2,60 +2,49 @@
 
 const input = await Deno.readTextFile("./input.txt");
 
-const stones = input.split(" ");
+const stones = input.split(" ").map((e) => parseInt(e));
 
 // Part 1
 
 const stonesMap: Map<number, number> = new Map();
 
-function initialize(map: Map<number, number>, stones: string[]) {
-  for (const stone of stones) {
-    const parsedStone = parseInt(stone);
+stones.forEach((stone) => {
+  const mappedStone = stonesMap.get(stone);
 
-    const mappedStone = map.get(parsedStone);
-
-    if (!mappedStone) {
-      map.set(parsedStone, 1);
-    } else {
-      map.set(parsedStone, mappedStone + 1);
-    }
-  }
-}
+  if (!mappedStone) stonesMap.set(stone, 1);
+  else stonesMap.set(stone, mappedStone + 1);
+});
 
 function doBlink(stonesMap: Map<number, number>) {
   for (const [stone, count] of [...stonesMap.entries()]) {
     if (count === 0) continue;
 
-    const stoneString = stone.toString();
+    if (stone === 0) {
+      const oneStoneCount = stonesMap.get(1) ?? 0;
 
-    if (stone == 0) {
-      const oneStoneCount = stonesMap.get(1);
-      stonesMap.set(1, (oneStoneCount ?? 0) + count);
-
-      const currentCount = stonesMap.get(stone)!;
-      stonesMap.set(stone, currentCount - count);
-    } else if (stoneString.length % 2 === 0) {
-      const halfIndex = stoneString.length / 2;
-
-      const firstHalf = Math.floor(stone / Math.pow(10, halfIndex));
-      const secondHalf = Math.floor(stone % Math.pow(10, halfIndex));
-
-      const firstHalfCount = stonesMap.get(firstHalf);
-      stonesMap.set(firstHalf, (firstHalfCount ?? 0) + count);
-
-      const secondHalfCount = stonesMap.get(secondHalf);
-      stonesMap.set(secondHalf, (secondHalfCount ?? 0) + count);
-
-      const currentCount = stonesMap.get(stone)!;
-      stonesMap.set(stone, currentCount - count);
+      stonesMap.set(1, oneStoneCount + count);
+      stonesMap.set(stone, (stonesMap.get(stone) ?? 0) - count);
     } else {
-      const newStone = stone * 2024;
+      const stoneString = stone.toString();
 
-      const newStoneCount = stonesMap.get(newStone);
-      stonesMap.set(newStone, (newStoneCount ?? 0) + count);
+      if (stoneString.length % 2 === 0) {
+        const halfIndex = stoneString.length / 2;
+        const firstHalf = Math.floor(stone / Math.pow(10, halfIndex));
+        const secondHalf = Math.floor(stone % Math.pow(10, halfIndex));
 
-      const currentCount = stonesMap.get(stone)!;
-      stonesMap.set(stone, currentCount - count);
+        const firstHalfCount = stonesMap.get(firstHalf) ?? 0;
+        stonesMap.set(firstHalf, firstHalfCount + count);
+
+        const secondHalfCount = stonesMap.get(secondHalf) ?? 0;
+        stonesMap.set(secondHalf, secondHalfCount + count);
+      } else {
+        const newStone = stone * 2024;
+        const newStoneCount = stonesMap.get(newStone) ?? 0;
+
+        stonesMap.set(newStone, newStoneCount + count);
+      }
+
+      stonesMap.set(stone, (stonesMap.get(stone) ?? 0) - count);
     }
   }
 }
@@ -63,8 +52,6 @@ function doBlink(stonesMap: Map<number, number>) {
 function blinkTimes(stonesMap: Map<number, number>, count: number) {
   for (let i = 0; i < count; i++) doBlink(stonesMap);
 }
-
-initialize(stonesMap, stones);
 
 blinkTimes(stonesMap, 25);
 
